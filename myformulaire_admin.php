5005462d01e96386a1c7fdbcd4d8107d81192883
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-08-31 11:06:35 +0200

    step 3 finish

--- myformulaire_admin.php
+++ myformulaire_admin.php
@@ -5,6 +5,7 @@ class MyFormulaire_Admin
     public function __construct()
     {
         add_action('admin_menu', array($this, 'addAdminMenu'));
+        add_shortcode('myformulaire_suscribers_list', array($this, 'shortcodeSuscribersList'));
     }
 
     public function addAdminMenu()
@@ -55,21 +56,7 @@ class MyFormulaire_Admin
     public function generateSuscribersHtml()
     {
         echo '<h1>' . get_admin_page_title() . '</h1>';
-
-        $suscribers = $this->getAllContacts();
-
-        if (count($suscribers) > 0) {
-            echo '<table class="my-formulaire-liste" style="border-collapse:collapse"><tbody>';
-            foreach ($suscribers as $suscriber) {
-                echo "<tr>
-                    <td width='150' style='border:1px solid black;'>{$suscriber->name}</td>
-                    <td width='300' style='border:1px solid black;'>{$suscriber->email}</td>
-                </tr>";
-            }
-            echo '<tbody></table>';
-        } else {
-            echo "<p>Not inscrit</p>";
-        }
+        echo $this->genHtmlList();
     }
 
     public function generateTestHtml()
@@ -77,6 +64,7 @@ class MyFormulaire_Admin
         echo '<h1>' . get_admin_page_title() .'</h1>';
         echo '<p style="color: red"> Coucou je suis un simple paragraphe pour tester d\'afficher une page </p>';
         var_dump($this->getAllContacts());
+        echo $this->genHtmlList();
     }
 
     private function getAllContacts()
@@ -85,5 +73,29 @@ class MyFormulaire_Admin
         $suscribers = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}my_formulaire");
 
         return $suscribers;
+    }
+
+    public function shortcodeSuscribersList($attr, $content)
+    {
+        $html = "<h2>Liste des inscrits</h2>";
+        if (isset($attr['subtitle'])) $html .= "<h3>{$attr['subtitle']}</h3>";
+        $html .= $this->genHtmlList();
+        return $html;
+    }
+
+    public function genHtmlList()
+    {
+        $suscribers = $this->getAllContacts();
+        $html = "";
+        if (count($suscribers) > 0) {
+            $html .= '<table class="my-formulaire-liste" style="border-collapse:collapse"><tbody>';
+            foreach ($suscribers as $suscriber) {
+                $html .= "<tr><td width='150' style='border:1px solid black;'>{$suscriber->name}</td><td width='300' style='border:1px solid black;'>{$suscriber->email}</td></tr>";
+            }
+            $html .= '<tbody></table>';
+        } else {
+            $html .= "<p>Not inscrit</p>";
+        }
+        return $html;
     }
 }
